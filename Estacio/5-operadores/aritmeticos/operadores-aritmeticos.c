e665a854a3443417e46731908f19a6c6c2129d12
Pedro0000000000 <tnthenriquepedro@gmail.com>
2025-02-13 15:53:19 -0300

    Criação dos arquivos que contem os arquivos sobre operadores matemaicos.

---
+++ Estacio/5-operadores/aritmeticos/operadores-aritmeticos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+  // Atribuição simples: (=)
+  // Atribuição com soma: (+=)
+  // Atribuição com subtração: (-=)
+  // Atribuição com multiplicação (*=)
+  // Atribuição com divisão (/=)
+  
+
+};
