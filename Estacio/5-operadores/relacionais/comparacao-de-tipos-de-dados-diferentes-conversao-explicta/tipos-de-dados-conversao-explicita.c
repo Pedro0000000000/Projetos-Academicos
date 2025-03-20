e7ca1995e181587d6f0d96c03e6d33adf149e1f1
Pedro0000000000 <tnthenriquepedro@gmail.com>
2025-03-20 00:21:06 -0300

    Criação da pasta "operadores > relacionais" que contem sub pastas sobre tipo de dados, conversão\explicita e comparação simples.

---
+++ Estacio/5-operadores/relacionais/comparacao-de-tipos-de-dados-diferentes-conversao-explicta/tipos-de-dados-conversao-explicita.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  float numero1 = 10.2f;
+  int numero2 = 10;
+
+  printf("Numero1 > numero2: %d\n", (int)numero1 > numero2);
+  printf("Numero1 == numero2: %d\n", (int)numero1 == numero2);
+
+  return 0;
+}
